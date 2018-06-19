8f0c65db8511716249b139678bc313bba3d8d2a4
livelybone <2631541504@qq.com>
2018-06-19 19:56:50 +0800

    add eslint

--- src/index.vue
+++ src/index.vue
@@ -1,128 +1,126 @@
 <template>
-    <div class="snack-bar-wrap" :class="wrapClass" :style="wrapClass?'':style.wrap">
-        <div v-for="(msg,i) in msgs"
-             class="snack-bar"
-             :style="wrapClass?'':style.bar(msg.color)"
-             @click="pop"
-             v-html="getMsg(msg.msg)">
-        </div>
+  <div class="snack-bar-wrap" :class="wrapClass" :style="wrapClass?'':style.wrap">
+    <div v-for="(msg,i) in msgs"
+         :key="i"
+         class="snack-bar"
+         :style="wrapClass?'':style.bar(msg.color)"
+         @click="pop"
+         v-html="getMsg(msg.msg)">
     </div>
+  </div>
 </template>
 
-<script>
-  export default {
-    name: 'SnackBar',
-    mounted() {
-    },
-    props: {
-      colors: {
-        default() {
-          return {
-            open: '#333',
-            info: '#3DBD7D',
-            error: '#FA7377',
-            warn: '#FF6600'
-          }
-        },
-        type: Object
-      },
-      holdTime: {
-        default: 3000,
-        type: Number
-      },
-      multiple: {
-        default: true,
-        type: Boolean
-      },
-      wrapClass: String,
-      baseSize: {
-        validator(val) {
-          return val === undefined || typeof val === 'string' && /\d(rem|px|em)$/.test(val)
-        }
-      },
-    },
-    data() {
-      return {
-        msgs: [],
-        color: ''
-      }
-    },
-    computed: {
-      style() {
-        return this.getStyle(this.baseSize)
-      }
-    },
-    methods: {
-      info(msg) {
-        this.validator(msg);
-        this.color = this.colors.info;
-        this.open({color: this.color, msg: msg})
-      },
-      error(msg) {
-        this.validator(msg);
-        this.color = this.colors.error;
-        this.open({color: this.color, msg: msg});
-        return false;
-      },
-      warn(msg) {
-        this.validator(msg);
-        this.color = this.colors.warn;
-        this.open({color: this.color, msg: msg});
-      },
-      open(msg) {
-        this.validator(msg);
-        const msgObj = typeof msg === 'string' ? {color: this.colors.open, msg} : msg;
-        if (this.multiple) this.msgs.push(msgObj);
-        else this.msgs = [msgObj];
-        setTimeout(() => this.pop(), parseInt(this.holdTime));
-      },
-      pop() {
-        this.msgs.splice(0, 1)
-      },
-      validator(msg) {
-        if (typeof msg !== 'string' && typeof msg.message !== 'string' && !(msg.toString instanceof Function)) {
-          throw new Error('Parameter msg is invalid. Expected a String, an Object with property toString[type:Function], or an Object with property message[type:String].')
-        }
-      },
-      getMsg(msg) {
-        if (typeof msg === 'string') return msg;
-        if (msg.message) return msg.name + ': ' + msg.message
-      },
-      getStyle(baseSize = '100px') {
+<script>export default {
+  name: 'SnackBar',
+  props: {
+    colors: {
+      default() {
         return {
-          wrap: {
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            flexWrap: 'wrap',
-            position: 'fixed',
-            left: '50%',
-            top: this.c('.05', baseSize),
-            zIndex: 1000,
-            width: 0,
-          },
-          bar: bg => ({
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            width: 'auto',
-            minWidth: baseSize,
-            maxWidth: `calc(100vw - .4 * ${baseSize})`,
-            minHeight: this.c(.5, baseSize),
-            padding: `${this.c(.1, baseSize)} ${this.c(.2, baseSize)}`,
-            margin: `0 0 ${this.c(.05, baseSize)}`,
-            borderRadius: this.c(.02, baseSize),
-            lineHeight: this.c(.2, baseSize),
-            color: '#fff',
-            background: bg,
-            boxShadow: `0 ${this.c(.01, baseSize)} ${this.c(.025, baseSize)} rgba(0,0,0, .15)`,
-            cursor: 'pointer',
-          })
-        }
+          open: '#333',
+          info: '#3DBD7D',
+          error: '#FA7377',
+          warn: '#FF6600',
+        };
       },
-      c(f, baseSize) {
-        return `calc(${f} * ${baseSize})`
+      type: Object,
+    },
+    holdTime: {
+      default: 3000,
+      type: Number,
+    },
+    multiple: {
+      default: true,
+      type: Boolean,
+    },
+    wrapClass: String,
+    baseSize: {
+      validator(val) {
+        return val === undefined || (typeof val === 'string' && /\d(rem|px|em)$/.test(val));
+      },
+    },
+  },
+  data() {
+    return {
+      msgs: [],
+    };
+  },
+  computed: {
+    style() {
+      return this.getStyle(this.baseSize);
+    },
+  },
+  methods: {
+    info(msg) {
+      this.validator(msg);
+      const color = this.colors.info;
+      this.open({ color, msg });
+    },
+    error(msg) {
+      this.validator(msg);
+      const color = this.colors.error;
+      this.open({ color, msg });
+      return false;
+    },
+    warn(msg) {
+      this.validator(msg);
+      const color = this.colors.warn;
+      this.open({ color, msg });
+    },
+    open(msg) {
+      this.validator(msg);
+      const msgObj = typeof msg === 'string' ? { color: this.colors.open, msg } : msg;
+      if (this.multiple) this.msgs.push(msgObj);
+      else this.msgs = [msgObj];
+      setTimeout(() => this.pop(), parseInt(this.holdTime, 10));
+    },
+    pop() {
+      this.msgs.splice(0, 1);
+    },
+    validator(msg) {
+      if (typeof msg !== 'string' && typeof msg.message !== 'string' && !(msg.toString instanceof Function)) {
+        throw new Error('Parameter msg is invalid. Expected a String, an Object with property toString[type:Function], or an Object with property message[type:String].');
       }
-    }
-  }
+    },
+    getMsg(msg) {
+      if (typeof msg === 'string') return msg;
+      if (msg.message && typeof msg.message === 'string') return msg.message;
+      return '';
+    },
+    getStyle(baseSize = '100px') {
+      return {
+        wrap: {
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexWrap: 'wrap',
+          position: 'fixed',
+          left: '50%',
+          top: this.c('.05', baseSize),
+          zIndex: 1000,
+          width: 0,
+        },
+        bar: bg => ({
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: 'auto',
+          minWidth: baseSize,
+          maxWidth: `calc(100vw - 0.4 * ${baseSize})`,
+          minHeight: this.c(0.5, baseSize),
+          padding: `${this.c(0.1, baseSize)} ${this.c(0.2, baseSize)}`,
+          margin: `0 0 ${this.c(0.05, baseSize)}`,
+          borderRadius: this.c(0.02, baseSize),
+          lineHeight: this.c(0.2, baseSize),
+          color: '#fff',
+          background: bg,
+          boxShadow: `0 ${this.c(0.01, baseSize)} ${this.c(0.025, baseSize)} rgba(0,0,0, .15)`,
+          cursor: 'pointer',
+        }),
+      };
+    },
+    c(f, baseSize) {
+      return `calc(${f} * ${baseSize})`;
+    },
+  },
+};
 </script>
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,28 @@
-const webpack = require('webpack');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const path = require('path');
 
 const config = {
-    entry: {index: './index.js'},
-    output: {
-      path: path.resolve(__dirname, './build'),
-      filename: './[name].js',
-      library: "VueSnackbar",
-      libraryTarget: "umd",
-      globalObject: 'this',
-    },
-    module: {
-      rules: [
-        {test: /\.vue$/, exclude: /node_modules/, loader: 'vue-loader'},
-        {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader', options: {presets: ['env']}},
-      ]
-    },
-    plugins: [
-      new VueLoaderPlugin()
-    ]
-  }
-;
-
+  entry: { index: './index.js' },
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: './[name].js',
+    library: 'VueSnackbar',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+  },
+  module: {
+    rules: [
+      { test: /\.vue$/, exclude: /node_modules/, loader: 'vue-loader' },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: { presets: ['env'] },
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
+};
 module.exports = config;
